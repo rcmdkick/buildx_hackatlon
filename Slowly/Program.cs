747921ccc32fb9fa3a1f70224c18b7cd72436581
RCMD-LaptopB <rcmd>
2025-03-02 14:13:54 +0100

    proper prompting, file save, "actual" folder

--- Slowly/Program.cs
+++ Slowly/Program.cs
@@ -9,26 +9,30 @@ namespace Slowly
     {
         private static readonly string apikeyFile = @"..\..\..\..\/apikeys/openai.txt";
         private static readonly string apiKey = File.ReadAllText(apikeyFile);
-        private static readonly string pdfPath = "..\\..\\..\\..\\lecture1.pdf"; // Path to your PDF file
-        private static readonly string PromptPath = "..\\..\\..\\..\\prompt.txt"; // Path to your prompt file
+        private static readonly string pdfPath = "..\\..\\..\\..\\/actual/lecture.pdf"; // Path to your PDF file
+        private static readonly string PromptPath = "..\\..\\..\\..\\prompts.txt"; // Path to your prompt file
+        private static readonly string outputPath = "..\\..\\..\\..\\/actual/output.json"; // Path to your prompt file
+        private static readonly string metadataPath = "..\\..\\..\\..\\/actual/metadata.json"; // Path to your prompt file
 
         static void Main(string[] args)
         {
-            
-
             ChatClient client = new(model: "gpt-4o", apiKey: apiKey);
             string extractedText = ExtractTextFromPdf(pdfPath);
 
             CollectionResult<StreamingChatCompletionUpdate> completionUpdates = client.CompleteChatStreaming(GetInstructionPrompt()+"\n\n"+extractedText);
-
+            string output = "";
             Console.Write($"[ASSISTANT]: ");
             foreach (StreamingChatCompletionUpdate completionUpdate in completionUpdates)
             {
+                string newUpdate = "";
                 if (completionUpdate.ContentUpdate.Count > 0)
                 {
-                    Console.Write(completionUpdate.ContentUpdate[0].Text);
+                    newUpdate += completionUpdate.ContentUpdate[0].Text;
+                    Console.Write(newUpdate);
+                    output += newUpdate;
                 }
             }
+            File.WriteAllText(outputPath, output);
         }
         static string ExtractTextFromPdf(string pdfPath)
         {
